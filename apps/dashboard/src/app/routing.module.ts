ab70c10a4020c93f6f5bef16445a41eda2ec5086
herdeybayor <shereefdeen4real@gmail.com>
2024-03-18 22:22:11 +0100

    refactor: update routing module

--- apps/dashboard/src/app/routing.module.ts
+++ apps/dashboard/src/app/routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { WidgetsComponent } from './widgets/widgets.component';
+import { HomeComponent } from './home/home.component';
 
-
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'widgets', component: WidgetsComponent },
+  { path: '**', redirectTo: '/' },
+];
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
-export class RoutingModule { }
+export class RoutingModule {}
