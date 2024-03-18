c59474865c1b6bfcef0c3ce25ecb36b600963b51
herdeybayor <shereefdeen4real@gmail.com>
2024-03-18 20:31:46 +0100

    feat: add widgets components

--- apps/dashboard/src/app/app.module.ts
+++ apps/dashboard/src/app/app.module.ts
@@ -12,9 +12,12 @@ import {
 import { MaterialModule, materialRoutes } from '@production-angular/material';
 import { CoreDataModule, coreDataRoutes } from '@production-angular/core-data';
 import { RoutingModule } from './routing.module';
+import { WidgetsComponent } from './widgets/widgets.component';
+import { WidgetsListComponent } from './widgets/widgets-list/widgets-list.component';
+import { WidgetsDetailsComponent } from './widgets/widgets-details/widgets-details.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, WidgetsComponent, WidgetsListComponent, WidgetsDetailsComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
