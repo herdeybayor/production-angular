c89c9431a6fac2d2116fff24aa13f6c6695e4853
herdeybayor <shereefdeen4real@gmail.com>
2024-03-20 23:02:00 +0100

    refactor: update home componet with widgets service

--- apps/dashboard/src/app/home/home.component.ts
+++ apps/dashboard/src/app/home/home.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { Widget } from '@production-angular/api-interfaces';
+import { WidgetsService } from '@production-angular/core-data';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'production-angular-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
-  widgets: Widget[] = [
-    { id: '1', title: 'Widget 01', description: 'Pending' },
-    { id: '2', title: 'Widget 02', description: 'Pending' },
-    { id: '3', title: 'Widget 03', description: 'Pending' },
-  ];
+export class HomeComponent implements OnInit {
+  widgets$!: Observable<Widget[]>;
+  constructor(private widgetsService: WidgetsService) {}
+
+  ngOnInit(): void {
+    this.loadWidgets();
+  }
+
+  loadWidgets() {
+    this.widgets$ = this.widgetsService.all();
+  }
 }
