d3c9f86492bb5d965e980a34254a41079ae5d867
herdeybayor <shereefdeen4real@gmail.com>
2024-03-20 23:01:34 +0100

    refactor: update client app

--- apps/client/src/app/app.component.ts
+++ apps/client/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Widget } from '@production-angular/api-interfaces';
+import { WidgetsService } from '@production-angular/core-data';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'production-angular-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  widgets$!: Observable<Widget[]>;
+  constructor(private widgetService: WidgetsService) {}
+
+  ngOnInit(): void {
+    this.widgets$ = this.widgetService.all();
+  }
 }
